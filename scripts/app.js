37b985656358b5193177f2f78d858d9a57fea56f
leegeunhyeok <lghlove0509@naver.com>
2019-01-08 23:20:51 +0900

    Add manifest.json

--- scripts/app.js
+++ scripts/app.js
@@ -193,6 +193,24 @@
     // request.send();
     
     // TODO add cache logic here
+    // if ('caches' in window) {
+    //   /*
+    //    * Check if the service worker has already cached this city's weather
+    //    * data. If the service worker has the data, then display the cached
+    //    * data while the app fetches the latest data.
+    //    */
+    //   caches.match(url).then(function(response) {
+    //     if (response) {
+    //       response.json().then(function updateFromCache(json) {
+    //         var results = json.query.results;
+    //         results.key = key;
+    //         results.label = label;
+    //         results.created = json.query.created;
+    //         app.updateForecastCard(results);
+    //       });
+    //     }
+    //   });
+    // }
 
     (function () {
       var todayWeatherCode = Math.floor(Math.random() * 50);
--- service-worker.js
+++ service-worker.js
@@ -16,8 +16,7 @@ var filesToCache = [
   '/images/scattered-showers.png',
   '/images/sleet.png',
   '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  '/images/thunderstorm.png'
 ];
 
 self.addEventListener('install', function(e) {
@@ -43,7 +42,7 @@ self.addEventListener('activate', function(e) {
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         // 이전에 캐시된 데이터 제거
-        if (key !== cacheName) {
+        if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
@@ -61,10 +60,34 @@ self.addEventListener('fetch', function(e) {
    * 
    * 브라우저의 기본 fetch 핸들링을 막고 스스로 Response에 대한 프라미스를 제공하도록 허용
    */
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      // 캐시에 해당 응답이 있을 경우 반환, 없을 경우 새로 fetch
-      return response || fetch(e.request);
-    })
-  );
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
